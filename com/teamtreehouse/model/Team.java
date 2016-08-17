ffccb3f00f745a491567129e906c9b9b1a512349
Daniel Vigil <daniel.julian.vigil@gmail.com>
2016-08-17 08:15:51 -0400

    added Team class

---
+++ com/teamtreehouse/model/Team.java
@@ -0,0 +1,32 @@
+package com.teamtreehouse.model;
+
+/**
+ * Created by danielvigil on 8/16/16.
+ */
+public class Team {
+
+    private String name;
+    private String coach;
+    private static final int MAX_MEMBERS = 11;
+
+    public Team(String name, String coach) {
+        this.name = name;
+        this.coach = coach;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getCoach() {
+        return coach;
+    }
+
+    public void setCoach(String coach) {
+        this.coach = coach;
+    }
+}
