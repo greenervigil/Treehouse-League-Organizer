0d6f0d9e4495c8659a584ca8c288b180e0843677
Daniel Vigil <daniel.julian.vigil@gmail.com>
2016-08-22 09:21:32 -0400

    completed add new Team function

--- LeagueManager.java
+++ LeagueManager.java
@@ -3,9 +3,7 @@ import com.teamtreehouse.model.Player;
 import com.teamtreehouse.model.Players;
 import com.teamtreehouse.model.Team;
 
-import java.io.IOException;
 
-import static com.teamtreehouse.model.Menu.menu;
 
 public class LeagueManager {
 
@@ -13,16 +11,9 @@ public class LeagueManager {
     Player[] players = Players.load();
     System.out.printf("There are currently %d registered players.%n", players.length);
     // Your code here!
-      System.out.printf("You can create %d teams.%n", (players.length / Team.MAX_MEMBERS));
-
-        Menu menu = new Menu();
-      try {
-          menu(players);
-      } catch (IOException e) {
-          e.printStackTrace();
-      }
-
-
+    System.out.printf("You can create %d teams.%n", (players.length / Team.MAX_MEMBERS));
+    Menu menu = new Menu(players);
+    menu.run();
   }
 
 }
--- com/teamtreehouse/model/Menu.java
+++ com/teamtreehouse/model/Menu.java
@@ -4,77 +4,115 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 /**
  * Created by danielvigil on 8/17/16.
  */
 public class Menu {
 
-    private static BufferedReader mReader = new BufferedReader(new InputStreamReader(System.in));;
-    private static List<Team> teams = new ArrayList<>();
+    private BufferedReader mReader;
+    private Player [] mPlayers;
+    private Map<String, String> mMenu;
+    private List<Team> mTeams;
 
-    //Main menu
-    public static void menu(Player [] players) throws IOException {
-        System.out.println("####################################");
-        System.out.println("############### Menu ###############");
-        System.out.println("####################################\n");
-        System.out.println("------ Organizer -------");
-        System.out.println("1 - Create New Team");
-        System.out.println("2 - Add Player to a Team");
-        System.out.println("3 - Remove Player From a Team");
-        System.out.println("-------- Coach ---------");
-        System.out.println("4 - Team Roster");
-        System.out.println("------- Reports --------");
-        System.out.println("5 - Team By Height");
-        System.out.println("6 - League Balance Report");
-        System.out.println("7 - Exit");
-        System.out.print("\nSelection:   ");
 
-        int input = Integer.parseInt(mReader.readLine());
-        while(input != 0 ) {
-            switch (input) {
-                case 1:
-                    createTeam(players);
-                    break;
-                case 2:
-                    //TODO Add a player to a team
-                    break;
-                case 3:
-                    //TODO Remove a player from an existing team
-                    break;
-                case 4:
-                    //TODO Display team roster
-                    break;
-                case 5:
-                    //TODO Team by height report
-                    break;
-                case 6:
-                    //TODO League Balance Report
-                    break;
-                case 7:
-                    System.exit(0);
-                default:
-                    System.out.println("I do not know what that is.  try again");
+    public Menu(Player [] players) {
+        mPlayers = players;
+        mTeams = new ArrayList<>();
+        mReader  = new BufferedReader(new InputStreamReader(System.in));
+        mMenu = new HashMap<>();
 
+        mMenu.put("1", "Create New Team");
+        mMenu.put("2", "Add Player to a Team");
+        mMenu.put("3", "Remove Player From a Team");
+        mMenu.put("4", "Team Roster");
+        mMenu.put("5", "Team By Height");
+        mMenu.put("6", "League Balance Report");
+        mMenu.put("7", "Exit");
+
+    }
+
+
+    public void run() {
+        String input = "";
+
+        do {
+            try {
+                input = promptAction();
+                switch (input) {
+                    case "1":
+                        System.out.println(checkAvailableTeams(mPlayers)
+                        + " " + mTeams.size());
+                        if (checkAvailableTeams(mPlayers)) {
+                            System.out.println("There is no more room for a new team. ");
+                        } else {
+                            promptNewTeam();
+                            System.out.println("Team added.");
+                        }
+                        break;
+                    case "2":
+                        //TODO Add a player to a team
+                        break;
+                    case "3":
+                        //TODO Remove a player from an existing team
+                        break;
+                    case "4":
+                        //TODO Display team roster
+                        break;
+                    case "5":
+                        //TODO Team by height report
+                        break;
+                    case "6":
+                        //TODO League Balance Report
+                        break;
+                    case "7":
+                        System.out.println("Good Bye!!");
+                        break;
+                    default:
+                        System.out.println("I do not know what that is.  try again");
+
+                }
+            } catch (IOException ioe) {
+                System.out.println("Problem with input.");
+                ioe.printStackTrace();
             }
-        }
 
+        } while (!input.equals("7"));
 
     }
 
-    public static List<Team> createTeam(Player [] players) throws IOException{
-        if(teams.size() <= (players.length / Team.MAX_MEMBERS)) {
-            System.out.println("What is the name of the new team: ");
-            String name = mReader.readLine();
-            System.out.println("Who is the coach:  ");
-            String coach = mReader.readLine();
-            Team team = new Team(name, coach);
-            teams.add(team);
-        }
-        return teams;
+
+    private Team promptNewTeam() throws IOException{
+        return new Team(promptForTeamName(), promptForCoachName());
     }
 
+    private String promptForTeamName() throws IOException{
+        System.out.println("What is the name of the new team: ");
+        return mReader.readLine();
+    }
 
+    private String promptForCoachName() throws IOException{
+        System.out.println("Who is the coach:  ");
+        return mReader.readLine();
+    }
+
+    private Boolean checkAvailableTeams(Player [] players){
+        if (mTeams == null || mTeams.size() == 0 ){
+            return false;
+        }
+        return mTeams.size() <= (players.length / Team.MAX_MEMBERS);
+    }
+
+    private String promptAction() throws IOException{
+        for (Map.Entry<String, String> option : mMenu.entrySet()) {
+            System.out.printf("%s - %s %n", option.getKey(), option.getValue());
+        }
+        System.out.print("What do you want to do: ");
+        String choice = mReader.readLine();
+        return choice.trim().toLowerCase();
+    }
 
 }
